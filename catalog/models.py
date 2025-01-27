c985ad683dfd236560fdddaad2487fa6479d0fad
Анна Кочерыгина <annvikky@gmail.com>
2025-01-28 01:15:46 +0800

    add branch features_2, models, migrations

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,67 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(
+        max_length=100,
+        verbose_name="Наименование",
+        help_text="Введите наименование продукта",
+    )
+    description = models.TextField(
+        verbose_name="Описание",
+        help_text="Введите описание продукта",
+        blank=True,
+        null=True,
+    )
+    photo = models.ImageField(
+        upload_to="images/",
+        verbose_name="Изображение",
+        help_text="Загрузите изображение",
+        blank=True,
+        null=True,
+    )
+    category = models.ForeignKey(
+        "Category",
+        on_delete=models.CASCADE,
+        verbose_name="Категория",
+        help_text="Выберите категорию",
+        related_name="products",
+    )
+    price = models.FloatField(
+        verbose_name="Цена",
+        help_text="Введите стоимость продукта",
+        blank=True,
+        null=True,
+    )
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length=100,
+        verbose_name="Наименование",
+        help_text="Введите наименование категории",
+    )
+    description = models.TextField(
+        verbose_name="Описание",
+        help_text="Введите описание категории",
+        blank=True,
+        null=True,
+    )
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
--- catalog/views.py
+++ catalog/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 
 
 def show_home(request):
-    """" Контроллер для отображения страницы home."""
-    return render(request, 'catalog/home.html')
+    """ " Контроллер для отображения страницы home."""
+    return render(request, "catalog/home.html")
 
 
 def show_contacts(request):
-    """" Контроллер для отображения страницы contacts."""
-    return render(request, 'catalog/contacts.html')
+    """ " Контроллер для отображения страницы contacts."""
+    return render(request, "catalog/contacts.html")
